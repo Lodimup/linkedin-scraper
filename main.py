bc55b4b12488d4505a5bb1f3d94aa7674db4aeb5
Pumidol Leelerdsakulvong <pumidol.le@42bangkok.com>
2022-04-28 20:33:22 +0700

    working build

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import os
+from discord_webhook import DiscordWebhook, DiscordEmbed
 from console import console
 from rich import inspect
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+load_dotenv()
 # from selenium.webdriver.common.keys import Keys
 import chromedriver_autoinstaller
 chromedriver_autoinstaller.install()
 
+WEBHOOK_URL = os.getenv('WEBHOOK_URL')
 
 driver = webdriver.Chrome()
 url = 'https://www.linkedin.com/jobs/search?keywords=developer&location=Bangkok%2C%20Bangkok%20City%2C%20Thailand&geoId=109988095&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&currentJobId=3038779681'
@@ -14,7 +20,7 @@ driver.get(url)
 content = driver.find_elements_by_class_name('base-card__full-link')
 
 class LJob:
-    def __init__(self, job_title, org_name):
+    def __init__(self, job_title, org_name, job_description):
         self.job_title = job_title
         self.org_name = org_name
         self.job_description = job_description
@@ -27,21 +33,36 @@ class LJob:
 
 l_ljobs = []
 for i in content:
-    link = i.get_attribute('href')
-    driver = webdriver.Chrome()
-    driver.get(link)
-    job_title = driver.find_element_by_class_name('top-card-layout__title').get_attribute('innerHTML')
-    org_name = driver.find_element_by_class_name('topcard__org-name-link').get_attribute('innerHTML')
-    btn = driver.find_element_by_class_name('show-more-less-html__button')
-    btn.click()
-    job_description = driver.find_element_by_class_name('show-more-less-html__markup').get_attribute('innerHTML')
-    job_description = BeautifulSoup(job_description, features='html.parser').get_text()
-    job_title = job_title.strip()
-    org_name = org_name.strip()
-    
-    ljob = LJob(job_title, org_name, job_description)
-    l_ljobs.append(ljob)
-    
-    driver.close()
+    try:
+        options = Options()
+        options.headless = True
+        
+        link = i.get_attribute('href')
+        link = link.split('?')[0]
+        driver = webdriver.Chrome(options=options)
+        driver.get(link)
+        job_title = driver.find_element_by_class_name('top-card-layout__title').get_attribute('innerHTML')
+        org_name = driver.find_element_by_class_name('topcard__org-name-link').get_attribute('innerHTML')
+        btn = driver.find_element_by_class_name('show-more-less-html__button')
+        btn.click()
+        job_description = driver.find_element_by_class_name('show-more-less-html__markup').get_attribute('innerHTML')
+        job_description = BeautifulSoup(job_description, features='html.parser').get_text()
+        job_title.strip()
+        org_name.strip()
+        
+        ljob = LJob(job_title, org_name, job_description)
+        l_ljobs.append(ljob)
+        
+        console.log(job_title)
+        webhook = DiscordWebhook(url=WEBHOOK_URL)
+        embed = DiscordEmbed(title=job_title, description=job_description[0:256], color='03b2f8')
+        console.log(f'')
+        embed.add_embed_field(name='Company name', value=org_name, inline=False)
+        embed.add_embed_field(name='Apply', value=link, inline=False)
+        webhook.add_embed(embed)
+        response = webhook.execute()
+        console.log(response.status_code)
 
-input('Press Enter to continue...')
+        driver.close()
+    except Exception as e:
+        print(e)
